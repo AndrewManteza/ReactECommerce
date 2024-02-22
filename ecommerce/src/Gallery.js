7e8619b887960caa6585998fd2ad63c2dee613f8
Andrew <manteza14@gmail.com>
2024-02-22 16:13:50 -0500

    reconfiguration

---
+++ ecommerce/src/Gallery.js
@@ -0,0 +1,19 @@
+function Gallery() {
+    return (
+      <img
+        src="C:\Users\Rosemarie Manteza\Desktop\Ecommerce\reactcommerce\ecommerce\public\Assets\Portfolio1.jpg"
+        alt="Portfolio1"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Gallery />
+        
+        
+      </section>
+    );
+  }
